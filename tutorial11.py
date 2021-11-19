d9c071cdfa845a3990f948784b0ad8798110aca5
heririta <heri.santoso308@gmail.com>
2021-11-19 19:44:12 +0700

    #11. slice operator

--- tutorial11.py
+++ tutorial11.py
@@ -1,1 +1,33 @@
-print("heri")
+#11. slice operator   
+
+fruits = ['apples','pears','strawberry']
+text = 'hello i like python'
+
+#print(text[start:stop:step])
+
+# print(text[2:5])
+
+# print(text[2:])
+
+# print(text[::2])
+
+# print(text[3::3])
+
+# fruits.append('bluberry')
+
+fruits[1:1] = 'b'
+
+print(fruits)
+
+
+
+
+
+
+
+
+
+
+
+
+
